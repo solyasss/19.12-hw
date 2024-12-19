a6ed676170bd9f57c3de1f313d558917b22ef17e
olya <olgastet01032005@gmail.com>
2024-12-19 11:20:43 +0200

    hw

---
+++ 19.12/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _19._12;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ 19.12/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace hw
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            loginButton.Click += login_click;
+            cancelButton.Click += cancel_click;
+        }
+        
+        private void login_click(object sender, RoutedEventArgs e)
+        {
+            string username = usernameTextBox.Text.Trim();
+            string password = passwordBox.Password.Trim();
+            
+            if (string.IsNullOrEmpty(username))
+            {
+                MessageBox.Show("Ready.", "Successfully", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            
+            MessageBox.Show("Ready.", "Successfully", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        
+        private void cancel_click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
